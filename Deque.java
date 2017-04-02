8490872981044c124433c5c6a91ad64dbbe5d71d
Sasha Fomina <Sasha Fomina>
2017-04-02 16:12:07 -0400

    interface

---
+++ Deque.java
@@ -0,0 +1,14 @@
+
+public interface Deque<E> {
+    boolean offerFirst(E e);
+    boolean offerLast(E e);
+    E pollFirst();
+    E pollLast();
+    E peekFirst();
+    E peekLast();
+    boolean removeFirstOccurrence(Object O);
+    boolean contains(object O);
+    int size();
+    //Iterator<E> iterator();
+    
+}
