237585e80aebbb2081cf879aecae8a1abe40e694
Sasha Fomina <Sasha Fomina>
2017-04-03 22:49:28 -0400

    comments and edits to iterator, descending iterator needs to be checked

--- DLDeque.java
+++ DLDeque.java
@@ -27,12 +27,14 @@ public class DLDeque<E> implements Deque<E>{
 	_size = 0;
     }
 
+    //adds an element to the front if the Deque has not yet reached capacity
+    //returns true if addition was successful, false otherwise
     public boolean offerFirst (E e){
 	if (_size == _capacity){
 	    return false;
 	}
 	if (_size == 0){
-	    DLLNode<E> addition = new DLLNode<E> (e,null,_front);
+	    DLLNode<E> addition = new DLLNode<E> (e,null,_front); //has a cargo of e and a next of _front
 	    _front = addition;
 	    _end = addition;
 	}
@@ -41,10 +43,13 @@ public class DLDeque<E> implements Deque<E>{
 	    _front.setPrev(addition);
 	    _front = addition;
 	}
-	_size++;
+	_front.setPrev(null); // previous of _front is always null
+	_size++; //increments size
 	return true;
     }
 
+    //adds an element to the end if the Deque has not yet reached capacity
+    //returns true if addition was successful, false otherwise 
     public boolean offerLast(E e){
 	if (_size == _capacity){
 	    return false;
@@ -55,14 +60,18 @@ public class DLDeque<E> implements Deque<E>{
 	    _end = addition;
 	}
 	else {
-	    DLLNode<E> addition = new DLLNode<E> (e, _end, null);
-	    _end.setNext(addition);
+	    DLLNode<E> addition = new DLLNode<E> (e, _end, null);//value of e, previous of _end and next of null
+	    _end.setNext(addition); 
 	    _end = addition;
 	}
-	_size++;
+	_front.setPrev(null);
+	_end.setNext(null); //next of end should always end up being null
+	_size++; //increment size 
 	return true;
     }
 
+    //removes an element from the front if size is not 0
+    //if size is 0 returns null, otherwise returns item removed
     public E pollFirst(){
 	if (_size == 0){
 	    return null;
@@ -71,11 +80,13 @@ public class DLDeque<E> implements Deque<E>{
 	    DLLNode<E> temp = _front;
 	    _front = _front.getNext();
 	    _front.setPrev(null);
-	    _size--;
+	    _size--; //size decremented 
 	    return temp.getCargo();
 	}
     }
 
+    //removes an element from the front if size is not 0
+    //if size is 0 returns null, otherwise returns item removed
     public E pollLast(){
 	if (_size == 0){
 	    return null;
@@ -84,11 +95,13 @@ public class DLDeque<E> implements Deque<E>{
 	    DLLNode<E> temp = _end;
 	    _end = _end.getPrev();
 	    _end.setNext(null);
-	    _size--;
+	    _size--; //size decremented 
 	    return temp.getCargo();
 	}
     }
 
+    //returns the cargo of the _front, if size is 0 returns null
+    //doesnt modify Deque
     public E peekFirst(){
 	if (_size ==0){
 	    return null;
@@ -96,6 +109,7 @@ public class DLDeque<E> implements Deque<E>{
 	return _front.getCargo();
     }
 
+    //returns cargo of _end
     public E peekLast(){
 	if(_size ==0){
 	    return null;
@@ -103,8 +117,8 @@ public class DLDeque<E> implements Deque<E>{
 	return _end.getCargo();
     }
     
-    //DOESN'T WORK
-    //fixed that ting yuhknow
+
+    //removes first occurrence starting from the end 
     public boolean removeFirstOccurrence( Object O )
     {
 	E searchVal = (E) O;
@@ -123,13 +137,16 @@ public class DLDeque<E> implements Deque<E>{
 			    {
 				temp.getPrev().setNext( temp.getNext() );
 			    }
+			_size --;
 			return true;
 		    }
 		temp = temp.getPrev();
 	    }
 	return false;
     }
-    
+
+    //returns true if a given object is found within the deque at least once
+    //returns false otherwise
     public boolean contains( Object O )
     {
 	E searchVal = (E) O;
@@ -146,7 +163,7 @@ public class DLDeque<E> implements Deque<E>{
 	return false;
     }
 			 
-
+    
     public String toString(){
 	String retStr = "_front --> ";
 	DLLNode<E> temp = _front;
@@ -162,6 +179,7 @@ public class DLDeque<E> implements Deque<E>{
 	return _size;
     }
 
+    //checks to see if deque has reached capacity
     public boolean isFull(){
 	return (_capacity == _size);
     }
@@ -203,10 +221,17 @@ public class DLDeque<E> implements Deque<E>{
 	//return next element in this iteration
 	public E next() 
 	{
-	    _dummy = _dummy.getNext();
-	    E ret = _dummy.getCargo();
-	    _okToRemove = true;
-	    return ret;
+	    System.out.println("SIZE: " + _size);
+	    if (_size == 1){
+		_okToRemove = true;
+		return null;
+	    }
+	    else {
+		_dummy = _dummy.getNext();
+		E ret = _dummy.getCargo();
+		_okToRemove = true;
+		return ret;
+	    }
 	}
 
 
@@ -223,17 +248,15 @@ public class DLDeque<E> implements Deque<E>{
 	    if ( _size == 1 ) {
 		_dummy = _dummy.getPrev();
 		_front = _end = null;
-		_dummy.setNext( _front );
 	    }
 	    
 	    //if removing first node...
 	    else if ( _dummy.equals( _front ) ) {
-		_dummy = _dummy.getPrev();
+		_dummy = _dummy.getNext();
 
 		_front = _front.getNext();
 		_front.setPrev( null );
-
-		_dummy.setNext( _front );
+		
 	    }
 
 	    //if removing last node...
@@ -270,7 +293,7 @@ public class DLDeque<E> implements Deque<E>{
 	{
 	    //place dummy node in front of head
             //...other housekeeping chores?
-	    _dummy = new DLLNode<E>( null, _end, null );
+	    _dummy = _end;
 	    _okToRemove = false;
 	}
 
@@ -285,10 +308,16 @@ public class DLDeque<E> implements Deque<E>{
 	//return next element in this iteration
 	public E next() 
 	{
-	    _dummy = _dummy.getPrev();
-	    E ret = _dummy.getCargo();
-	    _okToRemove = true;
-	    return ret;
+	    if(_size == 1){
+		_okToRemove = true;
+		return null;
+	    }
+	    else {
+		_dummy = _dummy.getPrev();
+		E ret = _dummy.getCargo();
+		_okToRemove = true;
+		return ret;
+	    }
 	}
 
 
@@ -303,35 +332,37 @@ public class DLDeque<E> implements Deque<E>{
 	    //maintain invariant that _dummy always points to a node
 	    //   (...so that hasNext() will not crash)
 	    if ( _size == 1 ) {
-		_dummy = _dummy.getNext();
+		//_dummy = _dummy.getNext();
 		_front = _end = null;
-		_dummy.setPrev( _end );
+		_dummy = null;
+		//_dummy.setPrev( _end );
 	    }
 	    
-	    //if removing first node...
+	    //if removing last node...
 	    else if ( _dummy.equals( _end ) ) {
-		_dummy = _dummy.getNext();
-
+		//System.out.println("SDFFFFFFFFFFFFFFF");
+		_dummy = _dummy.getPrev();
+		_dummy.setNext(null);
 		_end = _end.getPrev();
 		_end.setNext( null );
 
-		_dummy.setPrev( _end );
+
 	    }
 
-	    //if removing last node...
+	    //if removing first node...
 	    else if ( _dummy.equals( _front ) ) {
 		_dummy = _dummy.getNext();
 
 		_front = _front.getNext();
 		_front.setPrev(null);
 
-		_dummy.setPrev( _front );
+		//	_dummy.setPrev( _front );
 	    }
 
 	    //if removing an interior node...
 	    else {
 		_dummy = _dummy.getNext();		
-		_dummy.setPrev( _dummy.getPrev().getPrev() );
+		//_dummy.setPrev( _dummy.getPrev().getPrev() );
 		_dummy.getPrev().setNext( _dummy );
 	    }		     
 
@@ -359,6 +390,15 @@ public class DLDeque<E> implements Deque<E>{
 	System.out.println( sasha.contains( "b" ) );
 	sasha.removeFirstOccurrence( "b" ) ;
 	System.out.println( sasha );
+	System.out.println("TESTING ITERATOR");
+	Iterator stuff = sasha.descendingIterator();
+	stuff.next();
+	System.out.println(sasha);	
+	stuff.remove();
+	System.out.println(sasha);
+	//stuff.next();
+	//stuff.remove();
+	//System.out.println(sasha);
     }
     
 }
